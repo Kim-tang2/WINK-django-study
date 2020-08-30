809c190276f439beafa933622576e44a0e362f74
kim-tang2 <rlaxogns4504@gmail.com>
2020-08-31 02:50:10 +0900

    Update thumbnail model

--- wink_project/base/models.py
+++ wink_project/base/models.py
@@ -2,20 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Tag(models.Model):
-	name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
-class Post(models.Model):
-	headline = models.CharField(max_length=200)
-	sub_headline = models.CharField(max_length=200, null=True, blank=True)
-	body = models.TextField(null=True, blank=True)
-	created = models.DateTimeField(auto_now_add=True)
-	active = models.BooleanField(default=False)
-	featured = models.BooleanField(default=False)
-	tags = models.ManyToManyField(Tag, null=True, blank=True)
+class Post(models.Model):   
+    headline = models.CharField(max_length=200)
+    sub_headline = models.CharField(max_length=200, null=True, blank=True)
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="/images/placeholder.png")
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
 
-	def __str__(self):
-		return self.headline
+    def __str__(self):
+        return self.headline
