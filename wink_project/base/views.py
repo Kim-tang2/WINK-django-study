e9b474c4482c00fb71b6525fae73659e6bc580d8
kim-tang2 <rlaxogns4504@gmail.com>
2020-08-31 03:37:01 +0900

    Feature create post

--- wink_project/base/views.py
+++ wink_project/base/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .forms import PostForm
 
 from .models import Post
 # Create your views here.
@@ -7,13 +9,13 @@ from .models import Post
 def home(request):
     posts = Post.objects.filter(active=True, featured=True)[0:3]
 
-    context = {'posts':posts}
+    context = {'posts': posts}
     return render(request, 'base/index.html', context)
 
 def posts(request):
     posts = Post.objects.filter(active=True)
 
-    context = {'posts':posts}
+    context = {'posts': posts}
     return render(request, 'base/posts.html', context)
 
 def post(request, pk):
@@ -24,3 +26,15 @@ def post(request, pk):
 
 def profile(request):
     return render(request, 'base/profile.html')
+
+def createPost(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('posts')
+
+    context = {'form': form}
+    return render(request, 'base/post_form.html', context)
