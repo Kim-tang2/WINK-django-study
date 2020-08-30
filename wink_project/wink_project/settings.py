2daa7dbc97a5681d6128bd7abc7a11af70cf49ee
kim-tang2 <rlaxogns4504@gmail.com>
2020-08-31 01:45:50 +0900

    Update templete UI

--- wink_project/wink_project/settings.py
+++ wink_project/wink_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ztl#e+g9m!o$fos*^!vf*v0_tmi@&u55^a63=@u1q5)n&++&8w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
